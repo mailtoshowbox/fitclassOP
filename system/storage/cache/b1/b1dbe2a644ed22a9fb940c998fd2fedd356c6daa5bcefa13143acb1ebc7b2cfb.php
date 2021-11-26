48ba8bd056fe711dee889391eddd209bdb19900c
mailtoshowbox <mailtoshowbox@yahoo.com>
2021-11-26 15:50:25 +0530

    Initial commit

---
+++ system/storage/cache/b1/b1dbe2a644ed22a9fb940c998fd2fedd356c6daa5bcefa13143acb1ebc7b2cfb.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_5e286f9a2252e5746950427f439d4aa4b2c92d11bac4779d879185fc3d5568d6 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
